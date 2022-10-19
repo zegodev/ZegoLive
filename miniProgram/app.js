c76bf64206a024db7e8e00692109f0ca311bb9d7
“kritshuang” <kritshuang@zego.im>
2022-10-19 16:42:39 +0800

    修改app.js的配置，开发者需要自行指定生成token的URL

--- miniProgram/app.js
+++ miniProgram/app.js
@@ -1,5 +1,5 @@
 //app.js
-const liveAppID = 1739272706,//1252712950,
+const liveAppID = 1739272706, // appID
         appSign = "",  // appSign 不可随意暴露
     wsServerURL = "wss://wssliveroom-demo.zego.im/ws",//即构demo专用，开发者请填写即构邮件发送给你的
    logServerURL = "https://wsslogger-demo.zego.im/httplog";//可不填，sdk有配置时，配置的地址会覆盖这个地址,
@@ -8,7 +8,7 @@ App({
     globalData: {
         liveAppID,
         appSign,
-        tokenURL: "https://wssliveroom-demo.zego.im/token",
+        tokenURL: "", // 该接口由开发者后台自行实现，开发者的 Token 从各自后台获取。Token实现方式参考文档https://doc-zh.zego.im/article/2313，login_token生成示例代码部分。
         roomListURL: "https://liveroom1739272706-api.zego.im/demo/roomlist?appid=1739272706",//房间列表接口
         wsServerURL,
         logServerURL,
